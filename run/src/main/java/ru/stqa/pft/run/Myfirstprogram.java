15d20f2533bc376e4bd5fa2431d8e5033852f600
akozyreva1 <akozyreva@ujfusion.ru>
2020-06-25 02:33:34 +0300

    Дополнено несколько функций

--- run/src/main/java/ru/stqa/pft/run/Myfirstprogram.java
+++ run/src/main/java/ru/stqa/pft/run/Myfirstprogram.java
@@ -1,11 +1,27 @@
 package ru.stqa.pft.run;
 
+import javax.sql.rowset.serial.SQLOutputImpl;
+
 public class Myfirstprogram {
 
 	public static void main(String[] args){
-		
-		System.out.println("Hello, word!");
-		
+		hello("Alena");
+		hello("Alena");
+		hello("Alena");
+		double len=5;
+		System.out.println("Площадь квадрата со стороной " + len +" = "+ area (len));
+		double a=4;
+		double b=5;
+		System.out.println("Площадь прямоугольника со сторонами" + a + " и " + b + "=" + area (a, b));
 }
-
+public static void hello(String soody) {
+	System.out.println("Hello," +soody+ "!");
+}
+public static double area (double v )
+{
+	return  v*v;
+}
+public static double area (double a,double b){
+		return  a*b;
+}
 }
